fada95d6804c6130342981e528fa69b8e5679be5
AliPort <bwithtacos@gmail.com>
2022-09-26 21:39:59 -0500

    created associations

--- models/meet_greet.js
+++ models/meet_greet.js
@@ -4,13 +4,16 @@ const {
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
   class Meet_Greet extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
+        static associate({Band, Event}) {
+      Meet_Greet.belongsTo(Band, {
+        foreignKey: "band_id",
+        as: "band"
+      })
+
+      Meet_Greet.belongsTo(Event,{
+        foreignKey: "event_id",
+        as: "event"
+      })
     }
   }
   Meet_Greet.init({
--- models/set_time.js
+++ models/set_time.js
@@ -4,13 +4,21 @@ const {
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
   class Set_Time extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
+       static associate(Bands, Event, Stage) {
       // define association here
+      SetTime.belongsTo(Band, {
+        foreignKey: "band_id",
+        as: "band"
+      })
+    
+      SetTime.belongsTo(Event,{
+        foreignKey: "event_id",
+        as: "event"
+      })
+      SetTime.belongsTo(Stage, {
+        foreignKey: "stage_id",
+        as: "event"
+      })
     }
   }
   Set_Time.init({
